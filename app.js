4242ae30925fc37905095164f56f72796909837d
Vegaknak <125478434+Vegaknak@users.noreply.github.com>
2024-06-24 13:10:14 +0200

    2
    upload whole website

---
+++ app.js
@@ -0,0 +1,16 @@
+// fade animations
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }   else {
+            entry.target.classList.remove('show');
+        }
+
+    });
+
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
